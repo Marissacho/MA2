b391fecc15a88e6cf03817fbcb0ea8b7e39fd309
marissa <109318586+Marissacho@users.noreply.github.com>
2024-10-29 22:05:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,123 @@
-/*
-Inserting the randomly generated integers into a BST generally gives a height in
-the order of:
-Your answer:O(log n)
-*/
-#include <cstdlib> // For rand() and srand()
-#include <ctime> // For time()
-#include <chrono>
-#include <cmath>
-#include <random>
-#include "BST.h"
-double log2(double d)
-{
-return log(d) / log(2); // log() use e as base
+#include <map>
+#include "generateUsers.hpp"
+using namespace std;
+map<string, User> buildMapByUserName(vector<User> users) {
+map<string, User> usersMap;
+for (const auto& user:users){
+userMap[user.userName]=user;
 }
-void insertRandomIntegers(BST<int>* root, int numIntegers)
-{
-// The range of random integers
-const int minValue = -1000000000;
-const int maxValue = 1000000000;
-int randomInteger;
-// Generate a uniform distribution to generate random integers
-random_device dev;
-mt19937 rng(dev());
-uniform_int_distribution<std::mt19937::result_type> dist(minValue, maxValue);
-// Generate random integers (without duplicates) from the specified range
-for (int i = 0; i < numIntegers; ++i) {
-randomInteger = dist(rng);
-while ( root->contains(randomInteger) == true )
-{
-randomInteger = dist(rng);
+return usersMap;
 }
-root->insert(randomInteger);
+map<string, User> buildMapByEmail(vector<User> users) {
+map<string, User> emailMap;
+for (const auto& user:users){
+emailMap[user.userName]=user;
 }
+return emailMap;
+}
+void printMap(map<string, User> aMap) {
+cout << "Username: " <<key<<", User Details: "<<user<< endl;
+}
+bool isMapSorted(map<string, User> aMap) {
+if ( aMap.empty() ){
+return true;
+}
+auto i=aMap.begin();
+auto prev= i++;
+while(i!=aMap.end()){
+if(prev->first >i->first){
+return false;
+}
+prev=i++;
+}
+return true;
+}
+bool testSearchByKey(map<string, User> aMap, string keyToSearch) {
+return userMap.find(key)!=userMap.end();
+}
+bool testDeleteByKey(map<string, User> aMap, string keyToDelete) {
+return userMap.erase(key)>0;
+}
+void printActiveUsers(map<string, User> aMap) {
+int activeThreshold = 800;
+for(const auto&[userName, user]:aMap){
+if(user.tweets>activeThreshold){
+cout<<"Active User:"<<user.userName<<"with"<<user.tweets<<"tweets"<<endl;
+}
+}
+}
+void printMostPopularCategory(map<string, User> aMap) {
+std::map<std::string, int> categoryCount;
+for(const auto&[userName,user]:aMap){
+categoryCount[user.mostViewedCategory]++;
+}
+string mostPopularCategory;
+int maxCount=0;
+for(const auto&[category, count]: categoryCount){
+if(count>maxCount){
+maxCount= count;
+mostPopularCategory=category;
+}
+}
+cout<<"Most popular category:"<<mostPopularCategory<<endl;
 }
 int main()
 {
-// Seed the random number generator with the current time
-srand(static_cast<unsigned>(time(0)));
-// (i) test member functions with simple 6 inputs
-BST<int> *bst = new BST<int>();
-bst->insert(11);
-bst->insert(1);
-bst->insert(6);
-bst->insert(-1);
-bst->insert(-10);
-bst->insert(100);
-// a demo for findMin and findMax if "insert" is successfully implemented
-// cout << "(0) findMin = " << bst->findMin() << endl;
-// cout << " findMax = " << bst->findMax() << endl;
-cout << "(1.1) Print BST (in-order traversal): " << endl << " ";
-bst->printInOrder();
+int numUsers = 10;
+vector<User> users = generateUsers(numUsers);
+cout << "Build map with username as key" << endl;
+map<string, User> mapByUserName = buildMapByUserName(users);
+if ( mapByUserName.size() == numUsers )
+cout << " Built successfully." << endl << endl;
+else
+cout << " Built unsuccessfully." << endl << endl;
+cout << "Print \"mapByUserName\" map:" << endl;
+printMap(mapByUserName);
 cout << endl;
-cout << "(1.2) Print BST in level order: " << endl;
-bst->printLevels();
-cout << "(1.3) Is 100 in BST? true (1) or false (0): " << bst->contains(100) <<
-endl;
-cout << "(1.4) Is 9 in BST? true (1) or false (0): " << bst->contains(9) <<
-endl;
-cout << "(1.5) BST size: " << bst->treeSize() << endl;
-cout << "(1.6) Height of BST: " << bst->treeHeight() << endl;
-cout << "(1.7) Print max path: " << endl << " ";
-bst->printMaxPath();
+string keyToSearch = "smith55";
+cout << "Search by key: mapByUserName[\"" << keyToSearch << "\"]" << endl;
+if ( testSearchByKey(mapByUserName, keyToSearch) )
+cout << " Search successfully." << endl << endl;
+else
+cout << " Search unsuccessfully." << endl << endl;
+string keyToDelete = "smith55";
+cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+if ( testDeleteByKey(mapByUserName, keyToDelete) )
+cout << " Delete successfully." << endl << endl;
+else
+cout << " Delete unsuccessfully." << endl << endl;
+cout << "Test if map's key is sorted" << endl;
+if ( isMapSorted(mapByUserName) )
+cout << " Order test passed!" << endl << endl;
+else
+cout << " Order test failed!" << endl << endl;
+cout << "Print usernames with more than 800 tweets:" << endl;
+printActiveUsers(mapByUserName);
 cout << endl;
-bst->remove(11);
-cout << "(1.8) Removing 11, print BST (in-order traversal): " << endl << " ";
-bst->printInOrder();
+cout << "Print the most popular category" << endl;
+printMostPopularCategory(mapByUserName);
 cout << endl;
-cout << "(1.9) Print BST in level order: " << endl;
-bst->printLevels();
-cout << "(1.10) BST size: " << bst->treeSize() << endl;
-// Delete this bst
-delete bst;
+cout << "
+============================================================================== " <<
+endl << endl;
+cout << "Build map with username as key" << endl;
+map<string, User> mapByEmail = buildMapByEmail(users);
+if ( mapByEmail.size() == numUsers )
+cout << " Built successfully." << endl << endl;
+else
+cout << " Built unsuccessfully." << endl << endl;
+keyToSearch = "kat@gmail.com";
+cout << "Search by key: mapByEmail[\"" << keyToSearch << "\"]" << endl;
+if ( testSearchByKey(mapByEmail, keyToSearch) )
+cout << " Search successfully." << endl << endl;
+else
+cout << " Search unsuccessfully." << endl << endl;
+keyToDelete = "kat@gmail.com";
+cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+if ( testDeleteByKey(mapByEmail, keyToDelete) )
+cout << " Delete successfully." << endl << endl;
+else
+cout << " Delete unsuccessfully." << endl << endl;
 return 0;
+}
+
